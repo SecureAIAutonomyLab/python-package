55831df95f5040d476ba93fec742f6ed4248e91a
brandonwherry <bwh1126@gmail.com>
2023-06-05 23:22:46 -0500

    fixed b size

--- src/your_package/__main__.py
+++ src/your_package/__main__.py
@@ -27,7 +27,7 @@ def create_trainer(model: BertForSequenceClassification,
     training_args = TrainingArguments(
         output_dir='./results',
         num_train_epochs=1,
-        per_device_train_batch_size=32,
+        per_device_train_batch_size=16,
         per_device_eval_batch_size=64,
         warmup_steps=500,
         weight_decay=0.01,
