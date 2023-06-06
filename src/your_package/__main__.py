be5c876109cd82f883f4197700f232c967dc4989
brandonwherry <bwh1126@gmail.com>
2023-06-05 22:03:12 -0500

    changed epochs

--- src/your_package/__main__.py
+++ src/your_package/__main__.py
@@ -26,8 +26,8 @@ def create_trainer(model: BertForSequenceClassification,
                    eval_dataset: Dataset) -> Trainer:
     training_args = TrainingArguments(
         output_dir='./results',
-        num_train_epochs=3,
-        per_device_train_batch_size=16,
+        num_train_epochs=1,
+        per_device_train_batch_size=32,
         per_device_eval_batch_size=64,
         warmup_steps=500,
         weight_decay=0.01,
